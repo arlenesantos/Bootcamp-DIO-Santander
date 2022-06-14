20b6e250748f4616a33f625d0082985ba59186be
Arlene Santos <arlenesantos89@gmail.com>
2022-06-14 16:12:55 +0100

    typescript config

---
+++ modulo2/desafios-typescript/desafios-propostos/desafio2.js
@@ -0,0 +1,23 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}
